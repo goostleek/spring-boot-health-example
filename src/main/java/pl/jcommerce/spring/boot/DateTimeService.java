886ca6c395c66798652ca2f5ac70b6c24a0067d0
Marcin Kłopotek <marcin.klopotek@jcommerce.pl>
2016-02-01 16:39:39 +0100

    Hystrix circuit breaker example

--- src/main/java/pl/jcommerce/spring/boot/DateTimeService.java
+++ src/main/java/pl/jcommerce/spring/boot/DateTimeService.java
@@ -1,42 +1,49 @@
 package pl.jcommerce.spring.boot;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.Random;
+import javax.persistence.EntityManager;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.Cache;
+import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.CacheConfig;
-import org.springframework.cache.annotation.Cacheable;
-import org.springframework.http.HttpStatus;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.interceptor.SimpleKey;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
 @RestController
 @RequestMapping("/services")
-@CacheConfig(cacheNames = "dateTimeService")
+@CacheConfig(cacheNames = DateTimeService.DATE_TIME_SERVICE_CACHE)
 public class DateTimeService {
 
-	private final Random random = new Random();
+	static final String DATE_TIME_SERVICE_CACHE = "datetimeServiceCache";
 
-	@RequestMapping(value = "/datetime", method = RequestMethod.GET)
-	public ResponseEntity<?> getDateTime() throws InterruptedException {
-		final ResponseEntity<?> response;
-		if (random.nextInt(10) > 0) {
-			response = ResponseEntity.ok(LocalDateTime.now().toString());
-		} else {
-			final HttpStatus error = HttpStatus.INTERNAL_SERVER_ERROR;
-			response = ResponseEntity.status(error).body(error);
-		}
-		Thread.sleep(300);
-		return response;
+	private final Cache datetimeCache;
+
+	private final EntityManager em;
+
+	@Autowired
+	public DateTimeService(CacheManager cacheManager, EntityManager entityManager) {
+		datetimeCache = cacheManager.getCache(DATE_TIME_SERVICE_CACHE);
+		em = entityManager;
 	}
 
-	@Cacheable
-	@RequestMapping(value = "/date", method = RequestMethod.GET)
-	public ResponseEntity<String> getDate() throws InterruptedException {
+	@CachePut
+	@RequestMapping(value = "/datetime", method = RequestMethod.GET)
+	@HystrixCommand(fallbackMethod = "getCachedDateTime")
+	public ResponseEntity<String> getDateTime() throws InterruptedException {
 		Thread.sleep(300);
-		return ResponseEntity.ok(LocalDate.now().toString());
+		final Object dateTime = em.createNativeQuery("SELECT CURRENT_TIMESTAMP").getSingleResult();
+		return ResponseEntity.ok(dateTime.toString());
+	}
+
+	@SuppressWarnings("unchecked")
+	public ResponseEntity<String> getCachedDateTime() {
+		return datetimeCache.get(SimpleKey.EMPTY, ResponseEntity.class);
 	}
 
 }
--- src/main/java/pl/jcommerce/spring/boot/HealthExampleApplication.java
+++ src/main/java/pl/jcommerce/spring/boot/HealthExampleApplication.java
@@ -1,15 +1,19 @@
 package pl.jcommerce.spring.boot;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
 @EnableCaching
+@EnableCircuitBreaker
+@EnableHystrixDashboard
 public class HealthExampleApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HealthExampleApplication.class, args);
+		new SpringApplicationBuilder(HealthExampleApplication.class).web(true).run(args);
 	}
 
 }
