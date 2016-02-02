6478143d6899f1bf1f7d74df15a72529fb273791
Marcin Kłopotek <marcin.klopotek@jcommerce.pl>
2016-02-02 09:43:50 +0100

    Added metrics reporting to graphite server

--- src/main/java/pl/jcommerce/spring/boot/HealthExampleApplication.java
+++ src/main/java/pl/jcommerce/spring/boot/HealthExampleApplication.java
@@ -1,10 +1,20 @@
 package pl.jcommerce.spring.boot;
 
+import java.net.InetSocketAddress;
+import java.util.concurrent.TimeUnit;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.context.annotation.Bean;
+
+import com.codahale.metrics.MetricRegistry;
+import com.codahale.metrics.graphite.Graphite;
+import com.codahale.metrics.graphite.GraphiteReporter;
+import com.codahale.metrics.graphite.GraphiteSender;
 
 @SpringBootApplication
 @EnableCaching
@@ -16,4 +26,15 @@ public class HealthExampleApplication {
 		new SpringApplicationBuilder(HealthExampleApplication.class).web(true).run(args);
 	}
 
+	@Bean
+	public GraphiteReporter graphiteReporter(MetricRegistry metricRegistry, GraphiteSender graphiteSender) {
+		final GraphiteReporter reporter = GraphiteReporter.forRegistry(metricRegistry).build(graphiteSender);
+		reporter.start(1, TimeUnit.SECONDS);
+		return reporter;
+	}
+
+	@Bean
+	GraphiteSender graphite(@Value("${docker.host}") String dockerHost) {
+		return new Graphite(new InetSocketAddress(dockerHost, 2003));
+	}
 }
